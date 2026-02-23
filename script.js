74296a0317818c8338c4f657c905ce24eb8fcdd0
Scream-lx <captaintaothipphumirak@gmail.com>
2026-02-24 02:23:40 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// เปลี่ยนรูปใน Modal
-const galleryImages = document.querySelectorAll(".gallery-img");
-const modalImage = document.getElementById("modalImage");
-
-galleryImages.forEach(img=>{
-    img.addEventListener("click", function(){
-        modalImage.src = this.src;
-    });
-});
